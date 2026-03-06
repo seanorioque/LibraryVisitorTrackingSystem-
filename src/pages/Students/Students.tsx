15f43609faa4640c0a3be2cc0be94f870cbd3963
Sean Booker C. Orioque <seanorioque23@gmail.com>
2026-03-06 23:49:42 +0800

    seperated the path of admin and students

---
+++ src/pages/Students/Students.tsx
@@ -0,0 +1,5 @@
+const Student = ()=> {
+  return "asdas"
+}
+
+export default Student
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,15 @@
-declare module "*.png"  { const src: string; export default src; }
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
 
 interface ImportMetaEnv {
-  readonly VITE_FIREBASE_API_KEY:            string;
-  readonly VITE_FIREBASE_AUTH_DOMAIN:        string;
-  readonly VITE_FIREBASE_PROJECT_ID:         string;
-  readonly VITE_FIREBASE_STORAGE_BUCKET:     string;
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
-  readonly VITE_FIREBASE_APP_ID:             string;
+  readonly VITE_FIREBASE_APP_ID: string;
 }
 
 interface ImportMeta {
